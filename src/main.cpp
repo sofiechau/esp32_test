db92ca80bd20ba4762035b9cfec7f7af3951d3cf
sofiechau <scf138629@gmail.com>
2026-07-06 10:10:58 +0800

    i put the password and the servo hope it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Keypad.h>
+#include <ESP32Servo.h>
 
 const byte ROWS = 4;
 const byte COLS = 4;
@@ -13,16 +14,38 @@ char keys[ROWS][COLS] = {
 
 byte rowPins[ROWS] = {19,18,5,17};
 byte colPins[COLS] = {16,4,2,15};
+String input = "";
+String password = "1234";
+Servo myServo;
+const int servoPin = 13;
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("enter password");
-  String password = "1234";
+  myServo.attach(servoPin);
 }
 
 void loop() {
   char key = keypad.getKey();
+  if (key) {
+    Serial.println(key);
+    input += key;
+    if (input.length() == 4) {
+      if (input == password) {
+        Serial.println("Access granted");
+        myServo.write(0);
+        delay(2000);
+        myServo.write(90);
+        delay(2000);
+        myServo.write(0);
+        delay(2000);
+      } else {
+        Serial.println("Access denied");
+      }
+      input = "";
+    }
+  }
   
   
